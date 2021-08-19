1f8f0bd22dbf20e8c5412e44253c9c0cb336ce48
Viktor <mannar@inbox.ru>
2021-08-19 21:24:27 +0500

    Сокобан [1]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,6 +3,34 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+if ( Win.horizonta( board, i) == Win.vertical( board, i)){
+    rsl = false;
+}
+            }
+        }
         return rsl;
+    }
+
+    private   static  boolean horizonta( int [][] board, int index){
+        boolean rst = true;
+        for (int i = 0; i < board[index].length; i++) {
+            if (board[index][i] == 0) {
+                rst = false;
+            break;
+            }
+        }
+        return  rst;
+    }
+private   static  boolean vertical (int [][] board, int index){
+        boolean rsl = true;
+    for (int i = 0; i <board.length ; i++) {
+        if (board[i][index] == 0) {
+            rsl = false;
+        break;
+        }
+    }
+    return  rsl;
     }
 }
